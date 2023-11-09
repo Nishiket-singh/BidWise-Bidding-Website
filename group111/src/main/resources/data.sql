35e1ce7f4682c82006d0c3b01a601f946f3c1d20
khan-hashim <hashimak@my.yorku.ca>
2023-11-08 19:46:37 -0500

    schema and data changes

---
+++ group111/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, INITIALPRICE, TOTALTIME, AUCTIONTYPE, SHIPPINGTIME)
+VALUES
+('abc', 'xyz', 100, '3:00:00:00', 'Dutch', 14 ),
+('def', 'blah', 110, '4:00:00:00', 'Dutch', 14 ),
+('ghi', 'aah', 120, '2:23:59:59', 'Forward', 12 ),
+('jk', 'aahha', 130, '6:00:00:00', 'Forward', 7),
+('lm', 'aanoo', 140, '3:00:00:00', 'Forward', 8),
+('no', 'nooyes', 150, '7:00:00:00', 'Forward', 9),
+('pq', 'yessno', 160, '2:00:00:00', 'Dutch', 11 ),
+('rs', 'brpo8', 170, '2:00:00:00', 'Forward', 13 ),
+('t', 'brpo9', 180, '14:00:00:00', 'Dutch', 15 ),
+('xyz', 'brpo10', 190, '20:00:00:00', 'Forward',6);
